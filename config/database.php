a0af842d832918f2371a728cb6322156e8c67156
Lsangalang513 <127573800+S4NG4L4NG@users.noreply.github.com>
2024-10-14 23:00:35 +0800

    minor spelling mistek

--- config/database.php
+++ config/database.php
@@ -140,7 +140,7 @@ return [
         'cache' => [
             'scheme' => 'tls',
             'url' => env('REDIS_URL'),
-            'host' => env('ZURE_REDIS_HOST', '127.0.0.1'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
